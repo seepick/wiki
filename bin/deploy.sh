3803b473c9c1dd45fbe2f6e3ed234a9f3c51394a
Seepick <seepick@users.noreply.github.com>
2025-01-15 13:50:02 +0100

    auto-deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if [[ `pwd` == */bin ]]
+then
+    cd ..
+fi
+
 ./gradlew clean assemble && git add . && git commit -m "auto-deploy" && git push
 
 echo ""
 echo "Done ✅"
+echo ""
 echo "https://github.com/seepick/wiki/actions"
 echo "https://seepick.github.io/wiki/"
+
+exit 0
